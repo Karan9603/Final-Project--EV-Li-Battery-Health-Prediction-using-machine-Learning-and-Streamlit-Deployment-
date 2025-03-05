a1af7d3b15bdc659e8b402bf7f88173fb15b10f7
Karan9603 <karanvibhute058@gmail.com>
2025-03-05 16:10:43 +0100

    check

--- Streamlit_Battery_life_prediction_app.py
+++ Streamlit_Battery_life_prediction_app.py
@@ -3,24 +3,30 @@ import joblib
 import numpy as np
 import os
 
-st.write(f"Joblib version in Streamlit Cloud: {joblib.__version__}")
+# st.write(f"Joblib version in Streamlit Cloud: {joblib.__version__}")
 
-# Get current directory
-current_directory = os.getcwd()
-st.write(f"Current Directory: {current_directory}")
+# # Get current directory
+# current_directory = os.getcwd()
+# st.write(f"Current Directory: {current_directory}")
 
-# List all files in the directory
-st.write("Files in directory:", os.listdir(current_directory))
+# # List all files in the directory
+# st.write("Files in directory:", os.listdir(current_directory))
 
-# Check if the model file exists
-model_filename = "battery_health_L_model.joblib"
-model_path = os.path.join(current_directory, model_filename)
+# # Check if the model file exists
+# model_filename = "battery_health_L_model.joblib"
+# model_path = os.path.join(current_directory, model_filename)
 
-if os.path.exists(model_path):
-    st.write("Model file found. Loading...", model_path)
-    model = joblib.load(model_path)
-else:
-    st.error(f"Model file '{model_filename}' not found! Please check the file path.")
+# if os.path.exists(model_path):
+#     st.write("Model file found. Loading...", model_path)
+#     model = joblib.load(model_path)
+# else:
+#     st.error(f"Model file '{model_filename}' not found! Please check the file path.")
+    
+# Get the correct file path dynamically
+model_path = os.path.join(os.getcwd(), "battery_health_L_model.joblib")
+
+# Load the model
+model = joblib.load(model_path)
 
 # Load the trained model
 # model_path = r"C:\Users\Karan\Desktop\MY_Projects\Final-Project\battery_health_L_model.joblib"
